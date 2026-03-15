08a7d6740d79739cfbcdf1487c91b6f287ad467d
LilianLTran <lilianlinhtran@gmail.com>
2026-03-15 12:12:03 -0700

    Fix config

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,13 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
+  basePath: "/MyWebsite",
+  assetPrefix: "/MyWebsite/",
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
+
 };
 
 export default nextConfig;
